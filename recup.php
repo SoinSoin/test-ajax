7bac76df2b1943a6e61038f5815df65397954b2c
SoinSoin <vincent.retore@gmail.com>
2018-04-05 18:20:45 +0200

    v

--- recup.php
+++ recup.php
@@ -1,59 +1,34 @@
-    <?php
-// indiqué le chemin de votre fichier JSON, il peut s'agir d'une URL
+ <?php
 
-// function toto(){ 
-// echo(' {"cols": [{"id": "datejour","label":"", "type"="date"},
-//                             {"id":"fk_idhumeur","label":"", "type"="number"},
-//                             {"id":"total","label":"", "type"="number"}],
-//                             rows: [{c:[{v: "a"},');
 
 $BDD = "host=localhost port=5432 dbname=bdd_meteon user=admin  password=admin";
 pg_connect($BDD);
-$requete = pg_query("SELECT datejour, fk_idhumeur,count(fk_idhumeur) as \"total\" FROM  meteodujour inner join date on meteodujour.fk_iddate=date.iddate group by datejour, fk_idhumeur order by 1,2;");
-// $value=pg_fetch_array( $requete);
-$file="test.json";
-// print_r($value);
-while($value=pg_fetch_array( $requete)) { 
-//   echo  $value[0] ; 
-// $tablometeojour=array($value['datejour'], $value['fk_idhumeur'],$value['total']);
-//  $foo= json_encode($tablometeojour);
-//  echo $foo;
+$file = "test.json";
+$requete = pg_query("   ");
+echo '  {  "cols":[{ "type": "number"}],   ';
 
-$arra=
-array("rows"=> array(c=>array(v=>$value['datejour'],$value['fk_idhumeur'],$value['total'])));
-               
-                            $tab=json_encode($arra);
-echo $tab;
-
-file_put_contents($file,$tab);
-
-// print_r($arra);
-
-// $json["fk_idhumeur"]=$value['fk_idhumeur'];
-// $json["total"]=$value['total'];
-// echo("{");
-// echo("v:"); 
-echo $cara;
-// echo("}");
+echo' "rows": [';
+while ($value = pg_fetch_object($requete)) {
 
 
+
+    // $json = [c => [(object)[v => $value[1] ] , (object)[v => $value[2] ]] ];
+    // echo $value -> fk_idhumeur.",";
+    // $newtab = json_encode($json, true);
+    // echo $newtab;
 }
-// echo $string;
+echo "]";
+echo"}";
 
-// }
-// $test=toto();
-// $file="test.json";
-// file_put_contents($file,$test);
 
-// $string=file_get_contents("test.json");
-//   SELECT 
-//   datejour, fk_idhumeur,count(fk_idhumeur) as "total"
-//   FROM
-//   meteodujour
-//   inner join date on meteodujour.fk_iddate=date.iddate
-//   group by datejour, fk_idhumeur
-//   order by 1,2;
-// $encrjson=var_dump(json_decode($value));
-//  file_put_contents("test.json",$encrjson, FILE_APPEND | LOCK_EX);
+// SELECT array_to_json(t)
+// FROM (SELECT datejour, fk_idhumeur,count(fk_idhumeur) 
+// as "total" 
+// FROM  meteodujour 
+// inner join date on meteodujour.fk_iddate=date.iddate
+//  where date.iddate = 10 
+//  group  by datejour, fk_idhumeur order by 1,2) 
+// t;
+
 
 ?>
